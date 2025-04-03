6d098fd63eae3d0eaf1c2c2bcb42e71d964ae367
Monali2349 <78692439+Monali2349@users.noreply.github.com>
2025-04-03 06:22:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+//let score = "33abc"
+//let score = null
+//let score = undefined
+let score = true
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" -> 33
+// "33abc" -> NaN
+// null -> 0
+//true -> 1 ; 0-> false
+//undefined -> NaN
+
+let isLoggedIn = "Hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 -> true ; 0-> false
+//"" -> false
+//"Hitesh" -> true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
