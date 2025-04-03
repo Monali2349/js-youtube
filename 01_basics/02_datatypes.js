e0a22d6d89a783a033a5f4dc1e3a89cf66ae855d
Monali2349 <78692439+Monali2349@users.noreply.github.com>
2025-04-03 05:59:40 +0000

    datatypes and variables

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treat all JS code as newer version
+//alert(3+3) // we are using nodejs not browser
+console.log(3+3) // code readability should be high
+console.log("Hitesh")
+let name = "Hitesh"
+let age = 22
+let isLoggedIn = false
+console.log(typeof "Hitesh")
+console.log(typeof age)
+//number => 2 to power 53
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof "hitesh")
+console.log(typeof null)
+console.log(typeof undefined)
+
