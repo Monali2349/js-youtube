1a4613cac4a24cb56903e8734e781f1417a5b376
Monali2349 <78692439+Monali2349@users.noreply.github.com>
2025-04-06 13:02:51 +0000

    datatype-summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,32 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber)
+
+//**********Operations************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+ 
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1" + 2 + 2);
+console.log(1 + 2+ "2");
+console.log(+true);
+console.log(+"");
+
+let game = 100
+game++
+console.log(game)
---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,23 @@
+const score = 100
+const scoreVal = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber = 34565467893578444n;
+const heros = ["shaktiman","superman","batman"]
+let myObj = {
+    name:"Monali",
+    age:22,
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
